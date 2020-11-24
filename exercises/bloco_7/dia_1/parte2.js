9f2f31d47f66fcdbb95bb3ca23bc5fca94df84f3
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2020-11-24 15:36:07 -0300

    Exercício 2 da parte 2

--- exercises/bloco_7/dia_1/parte2.js
+++ exercises/bloco_7/dia_1/parte2.js
@@ -1,7 +1,25 @@
 //Exercício 1
 const factorial = n => {
-  for (let index = 1; index <= n; index++) {
-    let nFactorial = index * 1;
+  let nFactorial = 1;
+  for (let index = 2; index <= n; index++) {
+    nFactorial *= index;
   }
   return nFactorial;
 }
+console.log(factorial(4));
+
+//Exercício 2
+const longestWord = phrase => {
+  let words = phrase.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for (let index in words) {
+    if (words[index].length > maxLength) {
+      maxLength = words[index].length;
+      result = words[index];
+    }
+  }
+  return result;
+}
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
