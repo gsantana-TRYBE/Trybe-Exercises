f1eec3cab583e076e11cebec700fccdc303b6c87
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-11 16:47:33 -0300

    Exercício de fixação

---
+++ exercises/bloco_11/dia_1/exfixacao/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Eu from './component.js';
+
+function App() {
+  return <Eu />;
+}
+
+export default App;
---
+++ exercises/bloco_11/dia_1/exfixacao/src/component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Eu extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Gabriel</h1>
+        <p>Tenho 20 anos, sou de Goiânia e da turma 08 da Trybe</p>
+      </div>
+    )
+  }
+}
+export default Eu;
