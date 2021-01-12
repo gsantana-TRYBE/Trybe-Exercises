9b99419454b7cd4d4fcbbdcbaf797aaf67235e00
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-12 16:08:02 -0300

    Exercícios de fixação, 1 e 2

---
+++ exercises/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from './Image.js';
+import Order from './Order.js';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order user={energyDrink.user} product={energyDrink.product} price={energyDrink.price} />
+        <Order user={headphone.user} product={headphone.product} price={headphone.price} />
+      </div>
+    );
+  }
+}
+
+export default App;
