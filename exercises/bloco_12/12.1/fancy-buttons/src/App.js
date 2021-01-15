c766bee213d93bab71e5fd424f4c947402e417fe
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-15 16:11:43 -0300

    Exercícios de fixação 5,6 e 7

--- exercises/bloco_12/12.1/fancy-buttons/src/App.js
+++ exercises/bloco_12/12.1/fancy-buttons/src/App.js
@@ -5,17 +5,37 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+    this.state = {
+      numCliquesBotao1: 0,
+      numCliquesBotao2: 0,
+      numCliquesBotao3: 0
+    }
   }
-  handleClick() {
-    console.log(this);
-    console.log('Clicou');
+  handleClick1() {
+    this.setState((lastState) => ({
+      numCliquesBotao1: lastState.numCliquesBotao1 + 1
+    }))
+  }
+  handleClick2() {
+    this.setState((lastState) => ({
+      numCliquesBotao2: lastState.numCliquesBotao2 + 1
+    }))
+  }
+  handleClick3() {
+    this.setState((lastState) => ({
+      numCliquesBotao3: lastState.numCliquesBotao3 + 1
+    }))
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Botão</button>
+        <button onClick={this.handleClick1}>{this.state.numCliquesBotao1}</button>
+        <button onClick={this.handleClick2}>{this.state.numCliquesBotao2}</button>
+        <button onClick={this.handleClick3}>{this.state.numCliquesBotao3}</button>
       </div>
     )
   }
